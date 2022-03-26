ebe2193b6e3db9446f955d8f7c4a94219f2ba66a
Doron Brand <37496645+Dorbra@users.noreply.github.com>
2022-03-26 21:45:30 +0300

    Add files via upload

---
+++ kandula-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#aws eks update-kubeconfig --region us-east-1 --name dorbra-kandula-prod-23
+
+export ID="$(aws secretsmanager --region us-east-1 get-secret-value --secret-id kandula/aws/creds | jq -r '.SecretString' | jq -r '.ID')"
+
+export KEY="$(aws secretsmanager --region us-east-1 get-secret-value --secret-id kandula/aws/creds | jq -r '.SecretString' | jq -r '.KEY')"
+
+sed -i -e "s/AWSID/${ID}/g" kandula-deploy.yaml
+
+sed -i -e "s@AWSKEY@${KEY}@g" kandula-deploy.yaml
+
+kubectl apply -f kandula-deploy.yaml
+
+kubectl apply -f service.yaml
+
+# sed -i -e "s/${ID}/AWSID/g" kandula-deploy.yaml
+# sed -i -e "s@${KEY}@AWSKEY@g" kandula-deploy.yaml
